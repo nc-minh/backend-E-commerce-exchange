1ee2ded4a3fdb4c0c6a4ee1cd5b7de7a10c87751
nc-minh <nguyencongminh.official@gmail.com>
2022-01-04 03:31:32 +0700

    :art: update: api-update-profile(regexCheck)

--- src/app/controllers/update-profile-controllers.js
+++ src/app/controllers/update-profile-controllers.js
@@ -1,40 +1,52 @@
 const Accounts = require('../models/accounts')
-class UpdateProfileControllers{
-    updateProfile(req, res, next){
+class UpdateProfileControllers {
+    updateProfile(req, res, next) {
         const verify = req.verify
         const image = req.body.image
         const email = req.body.email
         const address = req.body.address
 
-        if(verify){
+        const REGEX = /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/
+        const regexCheck = REGEX.test(email)
+        if (image.length > 254 || email.length > 254 || address.length > 254) {
+            res.json({
+                message: 'Độ dài chuỗi kí tự quá lớn!',
+                status: 'failure'
+            })
+        } else if (!regexCheck) {
+            res.json({
+                message: 'Email bạn vừa nhập không đúng định dạng!',
+                status: 'failure'
+            })
+        } else if (verify) {
             Accounts.updateOne({
-                _id: verify._id
-            }, {
-                image: image,
-                email: email,
-                address: address
-            })
-            .then(data => {
-                console.log(data)
-                if(data.acknowledged){
-                    res.json({
-                        message: 'Cập nhật thông tin thành công!',
-                        status: 'success'
-                    })
-                }else{
-                    res.json({
-                        message: 'Data cần update gửi không đúng định dạng!',
-                        status: 'failure'
-                    })
-                }
-            })
-            .catch(err => {
-                console.log(err)
-                res.json({
-                    err: err
+                    _id: verify._id
+                }, {
+                    image: image,
+                    email: email,
+                    address: address
                 })
-            })
-        }else{
+                .then(data => {
+                    console.log(data)
+                    if (data.acknowledged) {
+                        res.json({
+                            message: 'Cập nhật thông tin thành công!',
+                            status: 'success'
+                        })
+                    } else {
+                        res.json({
+                            message: 'Data cần update gửi không đúng định dạng!',
+                            status: 'failure'
+                        })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.json({
+                        err: err
+                    })
+                })
+        } else {
             res.json('fail')
         }
     }
