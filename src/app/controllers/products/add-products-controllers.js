88487a0d9ab6fbbf2f5b4d2efe93f6de73b52885
nc-minh <nguyencongminh.official@gmail.com>
2022-01-07 03:33:47 +0700

    :feet: update: add-products api

--- src/app/controllers/products/add-products-controllers.js
+++ src/app/controllers/products/add-products-controllers.js
@@ -1,7 +1,45 @@
 const Products = require('../../models/products')
 class AddProductsControllers {
     addProducts(req, res, next){
-        res.send('ok')
+        const name = req.body.name
+        const price = req.body.price
+        const quantity = req.body.quantity
+        const img = req.body.img
+        const video = req.body.video
+        const category = req.body.category
+        const trademark = req.body.trademark
+        const sold = req.body.sold
+        const size = req.body.size
+        const discount = req.discount
+        const color = req.body.color
+        const description = req.body.description
+
+        Products.create({
+            name: name,
+            price: price,
+            quantity: quantity,
+            img: img,
+            video: video,
+            category: category,
+            trademark: trademark,
+            sold: sold,
+            size: size,
+            discount: discount,
+            color: color,
+            description: description
+        })
+        .then(data => {
+            console.log(data)
+            res.json({
+                data: data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({
+                err: err
+            })
+        })
     }
 }
 
