68aa4241a38e6278cf10c2a3feb9c49ce54a8fef
nc-minh <nguyencongminh.official@gmail.com>
2022-01-25 22:57:06 +0700

    :sparkles: add: api -> force delete user

--- src/routes/api-accounts-route.js
+++ src/routes/api-accounts-route.js
@@ -9,6 +9,7 @@ const updatePasswordControllers = require('../app/controllers/accounts/update-pa
 const getProfileControllers = require('../app/controllers/accounts/get-profile-controllers')
 const softDeleteAccControllers = require('../app/controllers/accounts/delete/soft-delete-acc-controllers')
 const restoreAccControllers = require('../app/controllers/accounts/delete/restore-acc-controllers')
+const forceDeleteAccControllers = require('../app/controllers/accounts/delete/force-delete-acc-controllers')
 //middlewares
 const checkRoleControllers = require('../app/middlewares/check-role')
 
@@ -26,5 +27,7 @@ router.get('/get-profile', checkRoleControllers.checkLogin, getProfileController
 router.delete('/soft-delete-acc/:id', checkRoleControllers.checkAdmin, softDeleteAccControllers.softDeleteAcc)
 
 router.patch('/restore-acc/:id', checkRoleControllers.checkAdmin, restoreAccControllers.restoreAcc)
+
+router.delete('/force-delete-acc/:id', checkRoleControllers.checkAdmin, forceDeleteAccControllers.forceDeleteAcc)
 
 module.exports = router
